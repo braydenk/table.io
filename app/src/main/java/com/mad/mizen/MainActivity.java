e9f6ed9609852ce3d2a2a6df1a0ac80a3089a4a5
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-10-01 12:56:38 +1000

    Created staff called button

--- app/src/main/java/com/mad/mizen/MainActivity.java
+++ app/src/main/java/com/mad/mizen/MainActivity.java
@@ -1,8 +1,15 @@
 package com.mad.mizen;
 
+import android.graphics.Color;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 import dagger.android.AndroidInjection;
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.support.HasSupportFragmentInjector;
@@ -18,6 +25,10 @@ public class MainActivity extends AppCompatActivity implements HasSupportFragmen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.configureDagger();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
     }
 
     @Override
@@ -27,5 +38,27 @@ public class MainActivity extends AppCompatActivity implements HasSupportFragmen
 
     private void configureDagger() {
         AndroidInjection.inject(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.call_staff:
+                Snackbar.make(findViewById(R.id.main_activity), "A staff member will be with you shortly", Snackbar.LENGTH_LONG).show();
+                break;
+            default:
+                break;
+        }
+
+        return true;
     }
 }
