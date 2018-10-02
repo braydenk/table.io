7b1d851e1021328e2bb614c5fc9b641cc42d7448
Brayden Killeen <18531092@students.latrobe.edu.au>
2018-10-02 16:38:30 +1000

    Added a RecyclerView to OrderFragment to display list of ordered items

--- app/src/main/java/com/mad/mizen/order/OrderFragment.java
+++ app/src/main/java/com/mad/mizen/order/OrderFragment.java
@@ -7,10 +7,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import com.mad.mizen.R;
 import com.mad.mizen.data.models.Order;
 import dagger.android.support.AndroidSupportInjection;
@@ -18,12 +20,16 @@ import javax.inject.Inject;
 
 public class OrderFragment extends Fragment {
 
+    @SuppressWarnings("unused")
+    private static final String TAG = OrderFragment.class.getSimpleName();
+
     @Inject
     ViewModelProvider.Factory viewModelFactory;
 
     OrderViewModel viewModel;
 
-    TextView textView;
+    RecyclerView recyclerView;
+    OrderRecyclerAdapter adapter;
 
     public OrderFragment() {}
 
@@ -33,7 +39,7 @@ public class OrderFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.order_fragment, container, false);
 
-        textView = view.findViewById(R.id.order_item_name);
+        recyclerView = view.findViewById(R.id.order_recycler_view);
 
         // Inflate the layout for this fragment
         return view;
@@ -42,6 +48,12 @@ public class OrderFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        adapter = new OrderRecyclerAdapter(getContext(), null);
+        recyclerView.setAdapter(adapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+
+        recyclerView.setLayoutManager(layoutManager);
 
         this.configureDagger();
         this.configureViewModel();
@@ -59,7 +71,8 @@ public class OrderFragment extends Fragment {
 
     private void updateOrder(Order order) {
         if (order != null) {
-            textView.setText(order.getItems().getName());
+            Log.d(TAG, "updateOrder: called");
+            adapter.updateOrder(order);
         }
     }
 
---
+++ app/src/main/java/com/mad/mizen/order/OrderRecyclerAdapter.java
@@ -0,0 +1,65 @@
+package com.mad.mizen.order;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.mad.mizen.R;
+import com.mad.mizen.data.models.Order;
+
+public class OrderRecyclerAdapter extends RecyclerView.Adapter<OrderRecyclerAdapter.ViewHolder> {
+
+    @SuppressWarnings("unused")
+    private static final String TAG = OrderRecyclerAdapter.class.getSimpleName();
+
+    private Context context;
+    private Order order;
+
+    OrderRecyclerAdapter(Context context, Order order) {
+        this.context = context;
+        this.order = order;
+    }
+
+    @NonNull
+    @Override
+    public OrderRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.order_item_row, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OrderRecyclerAdapter.ViewHolder holder, int position) {
+        holder.itemQuantity.setText(order.getItems().getQuantity());
+        holder.itemName.setText(order.getItems().getName());
+
+        Log.d(TAG, "onBindViewHolder: " + order.getItems().getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return -1;
+    }
+
+    public void updateOrder(Order order) {
+        this.order = order;
+        notifyDataSetChanged();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView itemQuantity;
+        private TextView itemName;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            itemQuantity = itemView.findViewById(R.id.order_item_quantity);
+            itemName = itemView.findViewById(R.id.order_item_name);
+        }
+    }
+}
